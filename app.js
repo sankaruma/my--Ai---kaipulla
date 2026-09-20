e713eb791030210981136fcf797e8e8e4f4905c3
uma sankar <uma.sankar73391@gmail.com>
2026-09-20 10:49:34 +0530

    Set working default Gemini API key

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
    ========================================================================== */
 
 // Global App State
-const GEMINI_API_KEY = "";
+const GEMINI_API_KEY = "AQ.Ab8RN6LZSsh8H2acLhGQBmY2WSo88fJq0E6rfnFDZBrJapJLtA";
 const state = {
     isUnlocked: false,
     isAuthenticated: false,
